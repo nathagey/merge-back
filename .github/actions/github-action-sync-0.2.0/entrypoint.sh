cb09f8c17c9ce2f54b9aa154b6b717aa986f823b
Nat Hagey <nhagey@glintinc.com>
2021-05-27 07:29:14 -0700

    try working with the branch directly and changing PR title

--- .github/actions/github-action-sync-0.2.0/entrypoint.sh
+++ .github/actions/github-action-sync-0.2.0/entrypoint.sh
@@ -31,13 +31,13 @@ last_sha=$(git_cmd git rev-list -1 upstream/${INPUT_UPSTREAM_BRANCH})
 echo "Last commited SHA: ${last_sha}"
 
 up_to_date=$(git_cmd git rev-list origin/${INPUT_BRANCH} | grep ${last_sha} | wc -l)
-pr_branch="merge/${INPUT_UPSTREAM_BRANCH}-to-${INPUT_BRANCH}-${last_sha}"
-pr_title="Chore Merge: ${INPUT_UPSTREAM_BRANCH} to ${INPUT_BRANCH}"
+# pr_branch="${INPUT_UPSTREAM_BRANCH}-to-${INPUT_BRANCH}-${last_sha}"
+pr_title="Chore: ${INPUT_UPSTREAM_BRANCH} ${last_sha}"
 
 if [[ "${up_to_date}" -eq 0 ]]; then
-  git_cmd git checkout -b "${pr_branch}" --track "upstream/${INPUT_UPSTREAM_BRANCH}"
-  git_cmd git push -u origin "${pr_branch}"
-  git_cmd git remote remove upstream
+  # git_cmd git checkout -b "${INPUT_UPSTREAM_BRANCH}" --track "upstream/${INPUT_UPSTREAM_BRANCH}"
+  # git_cmd git push -u origin "${pr_branch}"
+  # git_cmd git remote remove upstream
 
   hub pr list
   pr_exists=$(git_cmd hub pr list | grep ${last_sha} | wc -l)
@@ -46,7 +46,7 @@ if [[ "${up_to_date}" -eq 0 ]]; then
     echo "PR Already exists!!!"
     exit 0
   else
-    git_cmd hub pull-request -b "${INPUT_BRANCH}" -h "${pr_branch}" -l "${INPUT_PR_LABELS}" -a "${GITHUB_ACTOR}" -m "\"${pr_title}\""
+    git_cmd hub pull-request -b "${INPUT_BRANCH}" -h "${INPUT_UPSTREAM_BRANCH}" -l "${INPUT_PR_LABELS}" -a "${GITHUB_ACTOR}" -m "\"${pr_title}\""
   fi
 else
   echo "Branch up-to-date"
